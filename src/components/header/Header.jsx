cc1d1f529f497e90fa75c54a4e7f68dce7ed0b11
issamevead <ss.elmachehour@gmail.com>
2022-11-18 14:25:38 +0100

    stiil working on header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import "./header.css"
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hi, I'm</h5>
+        <h1>ELMACHEHOUR ISSAM</h1>
+        <h5 className='text-light'>I'm a Data Engineer</h5>
+        <CTA />
+        <div className="IMAGE_ME">
+          <img src={ME} alt="me" />
+        </div>
+      </div>
+    </header>
   )
 }
 
